9f3fc43b8debada2d674e0b59bf1ed3f833027b0
hhensley98 <hrhensley98@gmail.com>
2023-11-27 13:39:50 -0500

    Converting flowchart into tasks

---
+++ spidam.py
@@ -0,0 +1,5 @@
+# 1- Load audio file -> display the name of the file
+# 2 - If: the file is not .wav -> Convert to .wav -> 3
+# 3 - Else: Check for meta or multi channel
+# 4 -   True: Remove meta or handle multi channel -> 5
+# 5 -   False: Display time value of .wav in seconds
