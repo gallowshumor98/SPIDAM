d4d691018d02a32af7329ae863a36e9485c3b248
hhensley98 <hrhensley98@gmail.com>
2023-12-07 12:07:05 -0500

    Added hall clap .wav file to project

--- spidam.py
+++ spidam.py
@@ -3,3 +3,7 @@
 # 3 - Else: Check for meta or multi channel
 # 4 -   True: Remove meta or handle multi channel -> 5
 # 5 -   False: Display time value of .wav in seconds
+# 6 - Display wave form of .wav
+# 7 - Compute highest resonance and display frequency value in Hz
+# 8 - Compute Low, Mid, High frequency
+# 9 - Display plot of RT60 for Low, Mid, High frequecies
